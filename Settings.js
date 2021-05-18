49e1c5cfecc0d7b3afcead38aa95dc62e9a57fd0
Sanjay Jaiswal <55450200+sanjay-xdr@users.noreply.github.com>
2021-05-18 16:16:33 +0530

    adding the basic features and fixing bugs

--- Settings.js
+++ Settings.js
@@ -1,5 +1,118 @@
 const github = document.getElementById("github");
 const stackoverflow = document.getElementById("stackoverflow");
+const youtube = document.getElementById("youtube");
+const instagram = document.getElementById("instagram");
+
+const twitter = document.getElementById("twitter");
+
+const linkedin = document.getElementById("linkedin");
+
+const netflix = document.getElementById("netflix");
+
+const prime_video = document.getElementById("prime_video");
+
+const geeksforgeeks = document.getElementById("geeksforgeeks");
+
+
+const codeforces = document.getElementById("codeforces");
+
+
+const leetcode = document.getElementById("leetcode");
+
+const facebook = document.getElementById("facebook");
+
+const codechef = document.getElementById("codechef");
+
+
+
+const extra = document.getElementById("extra");
+
+const discord = document.getElementById("discord");
+
+const hackerearth = document.getElementById("hackerearth");
+
+
+
+extra.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ extra: extra.value }, function () {});
+});
+
+
+discord.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ discord: discord.value }, function () {});
+});
+
+
+
+facebook.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ facebook: facebook.value }, function () {});
+});
+
+
+
+codechef.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ codechef: codechef.value }, function () {});
+});
+
+
+hackerearth.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ hackerearth: hackerearth.value }, function () {});
+});
+
+
+
+leetcode.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ leetcode: leetcode.value }, function () {});
+});
+
+
+
+
+
+
+codeforces.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ codeforces: codeforces.value }, function () {});
+});
+
+
+geeksforgeeks.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ geeksforgeeks: geeksforgeeks.value }, function () {});
+});
+
+
+
+prime_video.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ prime_video: prime_video.value }, function () {});
+});
+
+
+netflix.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ netflix: netflix.value }, function () {});
+});
+
+
+
+linkedin.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ linkedin: linkedin.value }, function () {});
+});
+
+
+
+twitter.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ twitter: twitter.value }, function () {});
+});
+
+
+
+instagram.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ instagram: instagram.value }, function () {});
+});
+
+
+
+youtube.addEventListener("keyup", function () {
+  chrome.storage.sync.set({ youtube: youtube.value }, function () {});
+});
 
 github.addEventListener("keyup", function () {
   chrome.storage.sync.set({ github: github.value }, function () {});
@@ -50,5 +163,5 @@ function createNotification(){
 
     setTimeout(()=>{
         notif.remove()
-    },5000)
+    },2000)
 }
--- popup.js
+++ popup.js
@@ -1,13 +1,127 @@
-// var github = document.getElementById("github");
+var github = document.getElementById("github");
 
-// var stackoverflow = document.getElementById("stackoverflow");
+var stackoverflow = document.getElementById("stackoverflow");
+
+var youtube1 = document.getElementById("youtube1");
+
+var instagram = document.getElementById("instagram");
+
+var twitter = document.getElementById("twitter");
+
+var linkedin = document.getElementById("linkedin");
+
+var netflix = document.getElementById("netflix");
+
+var prime_video = document.getElementById("prime_video");
+
+var geeksforgeeks = document.getElementById("geeksforgeeks");
+
+var codeforces = document.getElementById("codeforces");
+
+var leetcode = document.getElementById("leetcode");
+
+var facebook = document.getElementById("facebook");
+
+var codechef = document.getElementById("codechef");
 
 
 
-// chrome.storage.sync.get(["github"], function (result) {
-//   github.href = result.github;
-// });
+var extra = document.getElementById("extra");
 
-// chrome.storage.sync.get(["stackoverflow"], function (result) {
-//   stackoverflow.href = result.stackoverflow;
-// });
+var discord = document.getElementById("discord");
+
+var hackerearth = document.getElementById("hackerearth");
+
+
+chrome.storage.sync.get(["hackerearth"], function (result) {
+  hackerearth.setAttribute("href", `${result.hackerearth}`)
+});
+
+
+
+chrome.storage.sync.get(["discord"], function (result) {
+  discord.setAttribute("href", `${result.discord}`)
+});
+
+
+
+chrome.storage.sync.get(["extra"], function (result) {
+  extra.setAttribute("href", `${result.extra}`)
+});
+
+
+
+
+chrome.storage.sync.get(["codechef"], function (result) {
+  codechef.setAttribute("href", `${result.codechef}`)
+});
+
+
+
+chrome.storage.sync.get(["facebook"], function (result) {
+  facebook.setAttribute("href", `${result.facebook}`)
+});
+
+
+
+
+chrome.storage.sync.get(["leetcode"], function (result) {
+  leetcode.setAttribute("href", `${result.leetcode}`)
+});
+
+
+
+chrome.storage.sync.get(["codeforces"], function (result) {
+  codeforces.setAttribute("href", `${result.codeforces}`)
+});
+
+
+chrome.storage.sync.get(["geeksforgeeks"], function (result) {
+  geeksforgeeks.setAttribute("href", `${result.geeksforgeeks}`)
+});
+
+
+
+
+chrome.storage.sync.get(["prime_video"], function (result) {
+  prime_video.setAttribute("href", `${result.prime_video}`)
+});
+
+
+chrome.storage.sync.get(["netflix"], function (result) {
+  netflix.setAttribute("href", `${result.netflix}`)
+});
+
+
+
+
+chrome.storage.sync.get(["linkedin"], function (result) {
+  linkedin.setAttribute("href", `${result.linkedin}`)
+});
+
+
+chrome.storage.sync.get(["twitter"], function (result) {
+  twitter.setAttribute("href", `${result.twitter}`)
+});
+
+
+
+chrome.storage.sync.get(["instagram"], function (result) {
+  instagram.setAttribute("href", `${result.instagram}`)
+});
+
+
+
+
+chrome.storage.sync.get(["youtube"], function (result) {
+  youtube1.setAttribute("href", `${result.youtube}`)
+});
+
+
+chrome.storage.sync.get(["github"], function (result) {
+  github.href = result.github;
+});
+
+chrome.storage.sync.get(["stackoverflow"], function (result) {
+  stackoverflow.href = result.stackoverflow;
+});
