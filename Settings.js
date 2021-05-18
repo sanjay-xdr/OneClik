4087d7296754124a8a0a88cd9e1b6d320f0d8441
Sanjay Jaiswal <55450200+sanjay-xdr@users.noreply.github.com>
2021-05-18 12:54:34 +0530

    adding toast notification

--- Settings.js
+++ Settings.js
@@ -8,3 +8,47 @@ github.addEventListener("keyup", function () {
 stackoverflow.addEventListener("keyup", function () {
   chrome.storage.sync.set({ stackoverflow: stackoverflow.value }, function () {});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// showing the Notification
+const button=document.querySelectorAll('.btn')
+const toasts=document.getElementById('toast')
+
+
+
+button.forEach(btn=>btn.addEventListener('click',()=>{
+    createNotification()
+}))
+
+
+function createNotification(){
+    const notif=document.createElement('div');
+    notif.classList.add('toast');
+    notif.innerText="Link added successfully ";
+    toasts.appendChild(notif);
+
+    setTimeout(()=>{
+        notif.remove()
+    },5000)
+}
